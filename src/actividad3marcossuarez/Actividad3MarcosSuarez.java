e5b62469d5fe174d3a3eeb9285f6c7b4b1606582
DAM125 <DAM125@RED13-04.iesmhp.local>
2017-11-30 15:19:32 +0100

    tipo simple

---
+++ src/actividad3marcossuarez/Actividad3MarcosSuarez.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad3marcossuarez;
+
+/**
+ *
+ * @author DAM125
+ */
+public class Actividad3MarcosSuarez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String cadena="un string cualquiera";
+       
+    }
+    
+}
